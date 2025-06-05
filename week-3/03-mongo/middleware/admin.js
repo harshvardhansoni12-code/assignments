2b27954587a4ac473d953182df12550832d452ae
harshvardhansoni12-code <sonihv01@gmail.com>
2025-06-05 18:38:29 +0530

    Update admin.js

--- week-3/03-mongo/middleware/admin.js
+++ week-3/03-mongo/middleware/admin.js
@@ -2,6 +2,21 @@
 function adminMiddleware(req, res, next) {
     // Implement admin auth logic
     // You need to check the headers and validate the admin from the admin DB. Check readme for the exact headers to be expected
+    const admin = req.headers.admin;
+    const username = req.headers.username;
+    user.findOne(
+        admin:"admin",
+        username:"username"
+    )
+    .then(if user(value){
+        res.send("valid users")
+        next();
+    }
+    else{
+        res.status(404).send(
+            "invalid users"
+        )
+    })
 }
 
 module.exports = adminMiddleware;
