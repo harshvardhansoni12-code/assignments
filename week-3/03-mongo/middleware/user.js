585bf706740f7f1d817a85151f102fc951f2cf6a
harshvardhansoni12-code <sonihv01@gmail.com>
2025-06-05 18:52:40 +0530

    Update user.js

--- week-3/03-mongo/middleware/user.js
+++ week-3/03-mongo/middleware/user.js
@@ -1,6 +1,21 @@
 function userMiddleware(req, res, next) {
     // Implement user auth logic
     // You need to check the headers and validate the user from the user DB. Check readme for the exact headers to be expected
+const username = req.headers.username;
+  const password = req.headers.password;
+
+  const user = userMiddleware
+    .findOne({ username: username, password: password })
+    .then(function (value) {
+      if (value) {
+        res.send("valid user");
+        next();
+      } else {
+        res.status(400).send("invalid user");
+      }
+    });
+
+
 }
 
 module.exports = userMiddleware;
